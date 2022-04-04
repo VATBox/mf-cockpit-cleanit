4a694ed76e98ff9413570fca02cf4e71d9140906
karomkrtchyan <mkrtchyan.karo@gmail.com>
2022-04-04 12:55:00 +0400

    fetch data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { EmptyRouteComponent } from "./empty-route/empty-route.component";
 
 @NgModule({
   declarations: [
-    AppComponent,
-    EmptyRouteComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
